e9714265796d5fd1be1fffc078260bf4b6ca088d
AdrienHq <adrienhecq@gmail.com>
2024-02-23 11:57:18 +0100

    feat: first entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,24 +2,46 @@
 
 namespace App\Entity;
 
+use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $information = null;
+    #[ORM\Column]
+    private ?bool $inStock = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $slug = null;
+    #[ORM\Column(nullable: true)]
+    private ?int $quantityInStock = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getName(): ?string
     {
@@ -38,33 +60,33 @@ class Product
         return $this->description;
     }
 
-    public function setDescription(string $description): static
+    public function setDescription(?string $description): static
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getInformation(): ?string
+    public function isInStock(): ?bool
     {
-        return $this->information;
+        return $this->inStock;
     }
 
-    public function setInformation(string $information): static
+    public function setInStock(bool $inStock): static
     {
-        $this->information = $information;
+        $this->inStock = $inStock;
 
         return $this;
     }
 
-    public function getSlug(): ?string
+    public function getQuantityInStock(): ?int
     {
-        return $this->slug;
+        return $this->quantityInStock;
     }
 
-    public function setSlug(string $slug): static
+    public function setQuantityInStock(?int $quantityInStock): static
     {
-        $this->slug = $slug;
+        $this->quantityInStock = $quantityInStock;
 
         return $this;
     }
@@ -77,6 +99,42 @@ class Product
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
 
         return $this;
     }
