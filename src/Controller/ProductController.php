c12bc8f025b3191b8a95b5ad9088db5e7cc792a4
AdrienHq <adrienhecq@gmail.com>
2024-02-23 19:07:17 +0100

    feat: simple logic first detail page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function PHPUnit\Framework\throwException;
 
 class ProductController extends AbstractController
 {
@@ -18,10 +19,14 @@ class ProductController extends AbstractController
     #[Route('/product/{slug}-{id}', name: "product.detail", requirements: ['id' => '\d+' , 'slug' => '[a-z0-9-]+'])]
     public function index(int $id, string $slug): Response
     {
+        $product = $this->productRepository->find($id);
+        if($product->getId() != $slug)
+        {
+            return $this->redirectToRoute("product.detail", ['slug' => $product->getSlug(), 'id' => $product->getId()]);
+        }
+
         return $this->render("Products/productDetail.html.twig",[
-            'id' => $id,
-            'slug' => $slug,
-            'product' => $this->productRepository->find($id)
+            'product' => $product
         ]);
     }
 }
