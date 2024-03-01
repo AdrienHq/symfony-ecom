d8d15326dbba59f52cb92ee8010be38d186edc27
AdrienHq <adrienhecq@gmail.com>
2024-03-01 11:56:56 +0100

    feat: webpack cong + local bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,12 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    // Define the entry for your custom JavaScript file
+    .addEntry('bootstrap', './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+    // Add app.scss as a style entry
+    .addStyleEntry('app', './assets/styles/app.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -54,7 +57,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
