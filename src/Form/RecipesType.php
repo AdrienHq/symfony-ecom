1ccd2a81d49f4319d0c6325a51270a72fd1d3f8b
AdrienHq <adrienhecq@gmail.com>
2024-03-03 17:31:19 +0100

    feat: Recipes form

---
+++ src/Form/RecipesType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipes;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('description')
+            ->add('questions')
+            ->add('recipeSteps')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('slug')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipes::class,
+        ]);
+    }
+}
