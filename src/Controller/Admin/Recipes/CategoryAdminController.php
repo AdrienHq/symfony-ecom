b5b5c97bfaf2a862bdf22a684b5f6a22848d754b
AdrienHq <adrienhecq@gmail.com>
2024-03-04 13:53:07 +0100

    feat: category CRUD

---
+++ src/Controller/Admin/Recipes/CategoryAdminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller\Admin\Recipes;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route("/admin/category", name: 'admin.category.')]
+class CategoryAdminController extends AbstractController
+{
+    public function __construct(
+        private readonly CategoryRepository $categoryRepository
+    )
+    {
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render("/admin/category/category.html.twig", [
+            'categories' => $this->categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('[/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Category $category, Request $request, EntityManagerInterface $em): RedirectResponse|Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'The category has been edited');
+            return $this->redirectToRoute('admin.category.index');
+        }
+        return $this->render('admin/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+            $this->addFlash('success', 'New category created');
+            return $this->redirectToRoute('admin.category.index');
+        }
+        return $this->render('admin/category/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(Category $category, EntityManagerInterface $em): RedirectResponse
+    {
+        $em->remove($category);
+        $em->flush();
+        $this->addFlash('success', 'Category deleted');
+        return $this->redirectToRoute('admin.category.index');
+    }
+}
