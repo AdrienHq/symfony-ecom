0dbe18931af7abb0c4abf7c3cf68c1f8a029f8a3
AdrienHq <adrienhecq@gmail.com>
2024-03-09 09:26:18 +0100

    feat: api POST recipes

--- src/Controller/Api/recipes/RecipesApiController.php
+++ src/Controller/Api/recipes/RecipesApiController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\Api\recipes;
 
+use App\DTO\recipes\RecipesPaginationDTO;
 use App\Entity\Recipes;
 use App\Repository\RecipesRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipesApiController extends AbstractController
 {
@@ -20,7 +23,7 @@ class RecipesApiController extends AbstractController
         ]);
     }
 
-    #[Route("/api/recipes")]
+    #[Route("/api/recipes", methods: ["GET"])]
     public function getCollectionRecipes(RecipesRepository $recipesRepository): JsonResponse
     {
         $recipes = $recipesRepository->findAll();
@@ -29,12 +32,35 @@ class RecipesApiController extends AbstractController
         ]);
     }
 
-    #[Route("/api/recipesPaginate")]
-    public function getCollectionForSpecificPageRecipes(RecipesRepository $recipesRepository, Request $request): JsonResponse
+    #[Route("/api/recipesPaginate", methods: ["GET"])]
+    public function getCollectionForSpecificPageRecipes(
+        RecipesRepository    $recipesRepository,
+        #[MapQueryString]
+        ?RecipesPaginationDTO $paginationDTO = null
+    ): JsonResponse
     {
-        $recipes = $recipesRepository->findRecipesForSpecificPage($request->query->getInt('page', 1));
+        $recipes = $recipesRepository->findRecipesForSpecificPage($paginationDTO->page);
         return $this->json($recipes, 200, [], [
             'groups' => ['recipes.getCollection', 'recipes.getCollectionPerPage']
+        ]);
+    }
+
+    #[Route("/api/recipes", methods: ["POST"])]
+    public function postRecipes(
+        Request       $request,
+        #[MapRequestPayload(
+            serializationContext: ['recipes.create']
+        )]
+        Recipes       $recipes,
+        EntityManager $em
+    ): JsonResponse
+    {
+        $recipes->setCreatedAt(new \DateTimeImmutable());
+        $recipes->setUpdatedAt(new \DateTimeImmutable());
+        $em->persist($recipes);
+        $em->flush();
+        return $this->json($recipes, 200, [
+            'groups' => ['recipes.get', 'recipes.getCollection']
         ]);
     }
 }
