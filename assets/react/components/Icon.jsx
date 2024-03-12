6d07cc9221b96d977d2d1cee81b6c59ebce0699b
AdrienHq <adrienhecq@gmail.com>
2024-03-12 15:15:37 +0100

    feat first react component using hooks

---
+++ assets/react/components/Icon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// export function Icon ({icon}) {
+//     return <i className={"fa-solid fa-" + icon} aria-hidden='true'></i>
+// }
+
+export function Icon({ icon }) {
+    return <i className={`fa-solid fa-${icon}`} aria-hidden='true'></i>;
+}
--- assets/react/controllers/Recipe.jsx
+++ assets/react/controllers/Recipe.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import {render} from "react-dom";
-
-// import useRecipes from '../hooks/useRecipes';
+import React, {useEffect} from 'react';
+import {unmountComponentAtNode} from "react-dom";
+import {createRoot} from 'react-dom/client';
+import {useRecipeFetch} from "../hooks/useRecipeFetch";
+import {Icon} from "../components/Icon";
 
 function Recipe() {
-    return <div>Hello tlm</div>
+    const {items: recipes, load, loading, count, hasMore} = useRecipeFetch('/api/recipes')
 
+    useEffect(() => {
+        load()
+    }, [])
+
+    return <div>
+        {loading && 'Loading ...'}
+        {JSON.stringify(recipes)}
+        <RecipesCount count={count}/>
+        {hasMore && <button disabled={loading} className="btn btn-primary" onClick={load}>Load more recipes</button> }
+    </div>
+
+}
+
+function RecipesCount({count}) {
+    return <h3>
+        <Icon icon="book"></Icon>
+        Number of recipes: {count}
+    </h3>
 }
 
 class RecipesElement extends HTMLElement {
     connectedCallback() {
-        render(<Recipe/>, this);
+        const root = createRoot(this);
+        root.render(<Recipe/>);
+    }
+
+    disconnectedCallback() {
+        unmountComponentAtNode(this)
     }
 }
 
