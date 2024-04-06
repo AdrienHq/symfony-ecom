9e1af70bfe0375e579623d0d1c4b0c4ab533563f
AdrienHq <adrienhecq@gmail.com>
2024-04-06 14:15:14 +0200

    feat: get and display comments with "pagination"

--- assets/react/controllers/Comment.jsx
+++ assets/react/controllers/Comment.jsx
@@ -5,8 +5,13 @@ import {useEffect} from "react";
 import {Icon} from "../components/Icon";
 import {createRoot} from "react-dom/client";
 
-function Comment() {
-    const {items: comments, load, loading, count} = usePaginatedFetch('/api/comments')
+const dateFormat = {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+}
+
+function Comments() {
+    const {items: comments, load, loading, count, hasMore} = usePaginatedFetch('/api/comments')
 
     useEffect(() => {
         load()
@@ -15,30 +20,53 @@ function Comment() {
     return (
         <div>
             {loading && 'Loading ...'}
-            { JSON.stringify(comments)}
             <Title count={count}/>
-            <button onClick={load}>Load the comments</button>
+            {comments.map(c => <Comment key={c.id} comment={c}/>)}
+            {hasMore &&
+                <button disabled={loading} className="btn btn-primary" onClick={load}>
+                    Load more comments
+                </button>
+            }
         </div>
     );
 }
 
-function Title({count}){
+function Title({count}) {
     return (
-        <h3>
-            <Icon icon={"comments"}/>
-            {count} comment{count > 1 ? 's' : ''}
+        <h3 className="mt-5">
+            <u>
+                Your comment{count > 1 ? 's' : ''} & question{count > 1 ? 's' : ''} ({count})
+            </u>
         </h3>
     )
 }
+
+const Comment = React.memo(({comment}) => {
+    const date = new Date(comment.publishedAt)
+    return (
+        <div className="row mt-5 ">
+            <div className="col-sm-9">
+                <h3><strong>{comment.author.username}</strong></h3>
+            </div>
+            <div className="col-sm-9">
+                <p>{comment.content}</p>
+            </div>
+            <div className="col-sm-9">
+                <h4>Posted at : <strong>{date.toLocaleString(undefined, dateFormat)}</strong></h4>
+            </div>
+        </div>
+    )
+
+})
 
 
 class CommentsElements extends HTMLElement {
     connectedCallback() {
         const root = createRoot(this);
-        root.render(<Comment/>);
+        root.render(<Comments/>);
     }
 
-    disconnectdCallback(){
+    disconnectdCallback() {
         unmountComponentAtNode(this)
     }
 
