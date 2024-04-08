7b979f3101dee16226675f9269285b6114fe13ba
AdrienHq <adrienhecq@gmail.com>
2024-04-08 16:51:38 +0200

    feat: Comments the old way

--- src/Controller/Recipes/RecipeController.php
+++ src/Controller/Recipes/RecipeController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Recipes;
 
 use App\Data\recipe\SearchData;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Form\recipe\SearchForm;
 use App\Repository\RecipesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,23 +16,44 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     public function __construct(
-        private readonly RecipesRepository $recipesRepository
+        private readonly RecipesRepository      $recipesRepository,
+        private readonly EntityManagerInterface $em
     )
     {
     }
 
-    #[Route('/recipe/{slug}-{id}', name: "recipe.detail", requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function index(int $id, string $slug): Response
+    #[Route('/recipe/{slug}-{id}', name: "recipe.detail", requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'], methods: ['GET', 'POST'])]
+    public function index(int $id, string $slug, Request $request): Response
     {
         $recipe = $this->recipesRepository->find($id);
         $recipes = $this->recipesRepository->findAll();
+
         if ($recipe->getSlug() != $slug) {
             return $this->redirectToRoute("recipe.detail", ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
+        $form = $this->createForm(CommentType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = (new Comment())
+                ->setAuthor($this->getUser())
+                ->setRecipe($recipe)
+                ->setContent($form->getData()['content']);
+            $this->em->persist($comment);
+            $this->em->flush();
+
+            //To empty the form (have a new one)
+            $form = $this->createForm(CommentType::class);
+
+            $this->addFlash('success', 'Your comment was posted !');
+            $this->redirectToRoute('recipe.detail', ['slug' => $slug, 'id' => $id]);
         }
 
         return $this->render("recipes/show.html.twig", [
             'recipe' => $recipe,
             'recipes' => $recipes,
+            'form' => $form->createView()
         ]);
     }
 
