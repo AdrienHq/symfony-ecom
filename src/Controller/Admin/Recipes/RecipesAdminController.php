68791d8687b8a272ea1ecd7b44f418747a743a86
AdrienHq <adrienhecq@gmail.com>
2024-05-25 18:28:36 +0200

    feat: new route to upload images

--- src/Controller/Admin/Recipes/RecipesAdminController.php
+++ src/Controller/Admin/Recipes/RecipesAdminController.php
@@ -7,11 +7,14 @@ use App\Form\recipe\RecipesType;
 use App\Repository\RecipesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route("/admin/recipes", name: 'admin.recipes.')]
 #[IsGranted('ROLE_ADMIN')]
@@ -73,5 +76,29 @@ class RecipesAdminController extends AbstractController
         $em->flush();
         $this->addFlash('success', 'Recipe deleted');
         return $this->redirectToRoute('admin.recipes.index');
+    }
+
+
+    #[Route('/upload-image', name: 'upload_image', methods: ['POST'])]
+    public function uploadImage(Request $request, SluggerInterface $slugger): JsonResponse
+    {
+        $file = $request->files->get('file');
+        if ($file) {
+            $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+            try {
+                $file->move(
+                    $this->getParameter('uploads_directory'),
+                    $newFilename
+                );
+                return new JsonResponse(['imageUrl' => '/uploads/'.$newFilename]);
+            } catch (FileException $e) {
+                return new JsonResponse(['error' => 'Failed to upload image'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new JsonResponse(['error' => 'No file uploaded'], Response::HTTP_BAD_REQUEST);
     }
 }
