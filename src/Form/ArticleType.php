83f6380a76ca0315f2969f238b0a281864e8815a
AdrienHq <adrienhecq@gmail.com>
2024-05-27 15:33:52 +0200

    feat: article form

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Recipes;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title'
+            ])
+            ->add('content', TextareaType::class, [
+                'required' => false,
+                'empty_data' => ''
+            ])
+            ->add('slug', TextType::class, [
+                'required' => false
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save'
+            ])
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->slugCompletion(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->timeStampGenerator(...));
+    }
+
+    public function slugCompletion(PreSubmitEvent $event): void
+    {
+        $data = $event->getData();
+        if (empty($data['slug'])) {
+            $slugger = new AsciiSlugger();
+            $data['slug'] = strtolower($slugger->slug($data['name']));
+        }
+        $event->setData($data);
+    }
+
+    public function timeStampGenerator(PostSubmitEvent $event): void
+    {
+        $data = $event->getData();
+        if (!$data instanceof Article) {
+            return;
+        }
+        $data->setUpdatedAt(new \DateTimeImmutable());
+        if (!$data->getId()) {
+            $data->setCreatedAt(new \DateTimeImmutable());
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
