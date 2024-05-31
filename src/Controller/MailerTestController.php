4a86325cfc30b2a24b88b50d152525fbf9fc8cf5
AdrienHq <adrienhecq@gmail.com>
2024-05-31 15:16:04 +0200

    feat: better test email

--- src/Controller/MailerTestController.php
+++ src/Controller/MailerTestController.php
@@ -6,16 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class MailerTestController extends AbstractController
 {
     private $mailer;
+    private $logger;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, LoggerInterface $logger)
     {
         $this->mailer = $mailer;
+        $this->logger = $logger;
     }
 
     #[Route('/test-email', name: 'test_email')]
@@ -27,8 +29,14 @@ class MailerTestController extends AbstractController
             ->subject('Test Email')
             ->text('This is a test email.');
 
-        $this->mailer->send($email);
-
-        return new Response('Email sent successfully');
+        try {
+            $this->logger->info('MAILER_DSN: ' . getenv('MAILER_DSN'));
+            $this->mailer->send($email);
+            return new Response('Email sent successfully');
+        } catch (\Exception $e) {
+            $this->logger->error('Email sending failed: '.$e->getMessage());
+            return new Response('Failed to send email: '.$e->getMessage(), 500);
+        }
     }
 }
+
