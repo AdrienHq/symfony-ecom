a152d97384fbe346ec22fda5a7c5ce33dd96b912
AdrienHq <adrienhecq@gmail.com>
2024-06-12 20:43:51 +0200

    feat: course create form translation en

--- src/Form/course/CourseType.php
+++ src/Form/course/CourseType.php
@@ -5,6 +5,7 @@ namespace App\Form\course;
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,13 @@ class CourseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'courseForm.name',
+                'translation_domain' => 'courseForm'
+            ])
             ->add('save', SubmitType::class, [
-                'label' => 'Send'
+                'label' => 'courseForm.save',
+                'translation_domain' => 'courseForm'
             ]);
         ;
     }
